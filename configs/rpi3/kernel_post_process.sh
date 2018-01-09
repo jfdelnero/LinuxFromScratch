741ea64c8f51da19d17a71fb59d66a094021b0f3
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-01-09 12:59:54 +0100

    Raspberry PI 3 target.

---
+++ configs/rpi3/kernel_post_process.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Cross compiler and Linux generation scripts
+# (c)2014-2018 Jean-François DEL NERO
+#
+# Raspberry pi 3 target kernel compilation
+# post process
+#
+
+source ${BASE_DIR}/configs/${TARGET_NAME}/config.sh || exit 1
+
+cp arch/${KERNEL_ARCH}/boot/Image ${TARGET_ROOTFS}/boot/Image || exit 1
+
+mkdir ${TARGET_ROOTFS}/boot/dts
+mkdir ${TARGET_ROOTFS}/boot/dts/broadcom
+mkdir ${TARGET_ROOTFS}/boot/dts/overlays
+
+cp arch/${KERNEL_ARCH}/boot/dts/broadcom/bcm*-rpi-3-b.dtb ${TARGET_ROOTFS}/boot/dts/broadcom
+cp arch/arm/boot/dts/overlays/*.dtb* ${TARGET_ROOTFS}/boot/dts/overlays/
+cp arch/arm/boot/dts/overlays/README ${TARGET_ROOTFS}/boot/dts/overlays/
