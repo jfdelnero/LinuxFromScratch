91cb6375a5e457105bbdbd49b8d33625c7b0996c
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-01-09 22:09:06 +0100

    Fix ssh keys path.

--- init_sd.sh
+++ init_sd.sh
@@ -44,7 +44,7 @@ cp -R ${BASE_DIR}/configs/${TARGET_NAME}/rootfs_cfg/usr/* ./usr/
 
 chmod +x ./etc/init.d/rcS
 chmod +x ./usr/share/udhcpc/*
-chmod go-rxw ./etc/ssh_host_rsa_key
+chmod go-rxw ./etc/ssh/ssh_host_*_key
 
 mkdir ramdisk
 mkdir mnt/tmp
