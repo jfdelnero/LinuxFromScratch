b7a571926198c6fe21b8968c0ba86910691c8c0c
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-01-09 12:58:11 +0100

    Clearfog Pro target.

---
+++ configs/clearfog/kernel_pre_process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Cross compiler and Linux generation scripts
+# (c)2014-2018 Jean-François DEL NERO
+#
+# Clearfog target kernel compilation
+#
+
+source ${BASE_DIR}/configs/${TARGET_NAME}/config.sh || exit 1
+
