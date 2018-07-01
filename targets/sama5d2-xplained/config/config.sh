f5e78c0ef859cf1cd9aca01a537821ebdfe1334c
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-07-01 11:14:25 +0200

    fix sama5d2-xplained target.

--- targets/sama5d2-xplained/config/config.sh
+++ targets/sama5d2-xplained/config/config.sh
@@ -17,10 +17,9 @@ export DEBUG_SUPPORT="1"
 export NETWORK_SUPPORT="1"
 export WIRELESS_SUPPORT="1"
 
-source ${BASE_DIR}/configs/common/config.sh || exit 1
+source ${BASE_DIR}/targets/common/config/config.sh || exit 1
 
 # Kernel
 
 SRC_PACKAGE_KERNEL=http://ftp.ntu.edu.tw/linux/kernel/v4.x/linux-4.9.30.tar.gz
-
 
