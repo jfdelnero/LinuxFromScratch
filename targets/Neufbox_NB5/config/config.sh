154708b2d723be1dee034ef711cdd4a6671da155
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2018-07-29 16:08:41 +0200

    New Neufbox NB5 target.

---
+++ targets/Neufbox_NB5/config/config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Cross compiler and Linux generation scripts
+# (c)2014-2018 Jean-François DEL NERO
+#
+# Neufbox NB5-SER-r1 target setup
+#
+
+export KERNEL_ARCH=arm
+export TGT_MACH=mips64-linux-gnueabi
+export SSL_ARCH=linux-mips64
+export GCC_ADD_CONF=""
+
+export KERNEL_IMAGE_TYPE="Image"
+
+export DEBUG_SUPPORT="1"
+export NETWORK_SUPPORT="1"
+export WIRELESS_SUPPORT="1"
+
+source ${BASE_DIR}/targets/common/config/config.sh || exit 1
+
+# Kernel
+
+SRC_PACKAGE_KERNEL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.17.9.tar.xz"
---
+++ targets/Neufbox_NB5/config/install_post_process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Cross compiler and Linux generation scripts
+# (c)2014-2018 Jean-François DEL NERO
+#
+# Neufbox NB5-SER-r1 post install process
+#
+
+source ${TARGET_CONFIG}/config.sh || exit 1
