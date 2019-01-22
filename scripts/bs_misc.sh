cf0ac05778a509f94cc7b64c1dc45a1b952067ab
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-01-22 18:20:37 +0100

    libcurl.

--- scripts/bs_misc.sh
+++ scripts/bs_misc.sh
@@ -390,6 +390,41 @@ then
 fi
 
 ####################################################################
+# Libcurl
+####################################################################
+
+CUR_PACKAGE=${SRC_PACKAGE_LIBCURL:-"UNDEF"}
+CUR_PACKAGE="${CUR_PACKAGE##*/}"
+if [ "$CUR_PACKAGE" != "UNDEF" ]
+then
+(
+	if [ ! -f ${TARGET_BUILD}/${CUR_PACKAGE}_DONE ]
+	then
+	(
+		unpack ${CUR_PACKAGE} ""
+
+		cd ${TARGET_BUILD} || exit 1
+		mkdir libcurl
+		cd libcurl || exit 1
+
+		${TARGET_SOURCES}/${TMP_ARCHIVE_FOLDER}/configure \
+				--prefix="${TARGET_ROOTFS}" \
+				--build=$MACHTYPE \
+				--host=$TGT_MACH \
+				--target=$TGT_MACH \
+				|| exit 1
+
+		make ${NBCORE} || exit 1
+		make ${NBCORE} install || exit 1
+
+		echo "" > ${TARGET_BUILD}/${CUR_PACKAGE}_DONE
+
+	) || exit 1
+	fi
+) || exit 1
+fi
+
+####################################################################
 # Uboot
 ####################################################################
 
