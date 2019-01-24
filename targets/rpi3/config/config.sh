ff1239e2252f4afa56cb91fc2bf2ad1ed03aca9b
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-01-24 10:34:27 +0100

    Cross compiler setup.

--- targets/rpi3/config/config.sh
+++ targets/rpi3/config/config.sh
@@ -31,6 +31,23 @@ export MESA_DRI_DRV="swrast"
 export MESA_GALLIUM_DRV="vc4,swrast"
 export DRM_SUPPORT="--enable-vc4"
 
+###################################################
+# Cross compiler for the cross compiled machine
+
+export CROSS_TGT_MACH=arm-none-eabi
+export CROSS_TGT_CPU=cortex-m3
+export CROSS_TGT_CPUMODE=thumb
+
+SRC_PACKAGE_TARGET_CROSS_BINUTILS="http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
+
+SRC_PACKAGE_TARGET_CROSS_GCC="http://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.gz"
+SRC_PACKAGE_TARGET_CROSS_GCC_MPFR="http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.xz"
+SRC_PACKAGE_TARGET_CROSS_GCC_GMP="http://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.xz"
+SRC_PACKAGE_TARGET_CROSS_GCC_MPC="http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
+SRC_PACKAGE_TARGET_CROSS_GCC_ISL="ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2"
+SRC_PACKAGE_TARGET_CROSS_GCC_CLOOG="ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz"
+
+###################################################
 # Kernel
 
 SRC_PACKAGE_KERNEL="https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20180417-1.tar.gz"
