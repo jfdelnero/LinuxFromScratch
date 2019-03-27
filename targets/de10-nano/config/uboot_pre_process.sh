9a7c14fac7023a68c6f5ea043f52e1efea6011b0
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-03-27 22:18:06 +0100

    remove uboot patchs and use default config.

--- targets/de10-nano/config/uboot_pre_process.sh
+++ targets/de10-nano/config/uboot_pre_process.sh
@@ -8,11 +8,3 @@
 
 source ${TARGET_CONFIG}/config.sh || exit 1
 
-patch -i ${TARGET_CONFIG}/patchs/0001-arm-socfpga-Move-CONFIG_EXTRA_ENV_SETTINGS-to-common.patch -p1 || exit 1
-patch -i ${TARGET_CONFIG}/patchs/0002-arm-socfpga-Update-DE0-Nano-SoC-to-support-distro-bo.patch -p1 || exit 1
-patch -i ${TARGET_CONFIG}/patchs/0003-arm-socfpga-add-support-for-Terasic-DE10-Nano-board.patch -p1 || exit 1
-patch -i ${TARGET_CONFIG}/patchs/0004-Add-HDMI-init-to-de10-env.patch -p1 || exit 1
-patch -i ${TARGET_CONFIG}/patchs/0005-Add-DE10-Nano-HDMI-configuration-and-debug-apps.patch -p1 || exit 1
-patch -i ${TARGET_CONFIG}/patchs/0006-arm-socfpga-fix-issue-with-warm-reset-when-CSEL-is-0.patch -p1 || exit 1
-cp ${TARGET_CONFIG}/patchs/socfpga_de10_nano.h include/configs/
-
