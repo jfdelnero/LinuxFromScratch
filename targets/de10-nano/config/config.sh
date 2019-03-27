c769ecdaf76cde6aef9ac5313d885623df96aef3
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-03-27 22:22:22 +0100

    use altera uboot version. altera & project path.

--- targets/de10-nano/config/config.sh
+++ targets/de10-nano/config/config.sh
@@ -23,10 +23,21 @@ export NETWORK_STATION_MODE="1"
 
 source ${BASE_DIR}/targets/common/config/config.sh || exit 1
 
+# Altera tools installation path
+
+export ALTERA_TOOLS_ROOT="/home/jeff/intelFPGA_lite/18.1/"
+
+# GHRD FPGA PROJECT folder
+export FPGA_GHRD_FOLDER=/home/jeff/dev/DE10-Nano/DE10-Nano_v.1.3.4_HWrevC_SystemCD/Demonstrations/SoC_FPGA/DE10_NANO_SoC_GHRD
+
 # Kernel
 
 SRC_PACKAGE_KERNEL="https://github.com/altera-opensource/linux-socfpga/archive/rel_socfpga-4.14.73-ltsi-rt_19.03.01_pr.tar.gz"
-SRC_PACKAGE_UBOOT="https://github.com/u-boot/u-boot/archive/v2017.03.tar.gz"
+
+#uboot
+export UBOOT_DEFCONF=socfpga_cyclone5_config
+SRC_PACKAGE_UBOOT="https://github.com/altera-opensource/u-boot-socfpga/archive/rel_socfpga_v2013.01.01_19.03.02_pr.tar.gz"
+
 SRC_PACKAGE_FTRACE="http://ftp.debian.org/debian/pool/main/t/trace-cmd/trace-cmd_2.4.0.orig.tar.gz"
 SRC_PACKAGE_VALGRIND=
 
