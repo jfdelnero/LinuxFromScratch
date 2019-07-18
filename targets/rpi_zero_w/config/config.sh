bed37b52be125cd833e5608019ccbfa90b30eabf
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-07-18 22:02:35 +0200

    RPI w zero -> kernel 4.19

--- targets/rpi_zero_w/config/config.sh
+++ targets/rpi_zero_w/config/config.sh
@@ -12,14 +12,31 @@ export SSL_ARCH=linux-armv4
 export GCC_ADD_CONF=""
 
 export KERNEL_IMAGE_TYPE="Image"
+export KERNEL_DTBS="YES"
+export KERNEL_FIRMWARES="YES"
+
+export NETWORK_ROUTER_MODE="YES"
 
 export DEBUG_SUPPORT="1"
+export TARGET_BUILD_SUPPORT="1"
+
 export NETWORK_SUPPORT="1"
 export WIRELESS_SUPPORT="1"
+#export GFX_SUPPORT="1"
+#export WAYLAND_SUPPORT="1"
+#export AUDIO_SUPPORT="1"
+#export GAMES_SUPPORT="1"
 
-source ${BASE_DIR}/configs/common/config.sh || exit 1
+source ${BASE_DIR}/targets/common/config/config.sh || exit 1
+
+#export MESA_DRI_DRV="swrast"
+#export MESA_GALLIUM_DRV="vc4,swrast"
+#export DRM_SUPPORT="--enable-vc4"
 
 # Kernel
 
-SRC_PACKAGE_KERNEL="https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20180417-1.tar.gz"
+SRC_PACKAGE_KERNEL="https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20190709-1.tar.gz"
+
+SRC_PACKAGE_PERL=
+SRC_PACKAGE_PERLCROSS=
 
