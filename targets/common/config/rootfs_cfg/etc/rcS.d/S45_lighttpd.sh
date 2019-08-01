ae713222d0bf330dcb36fd4cd0514ed1642e96b2
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2019-08-01 15:10:07 +0200

    lighttpd : Fix POST / upload support

--- targets/common/config/rootfs_cfg/etc/rcS.d/S45_lighttpd.sh
+++ targets/common/config/rootfs_cfg/etc/rcS.d/S45_lighttpd.sh
@@ -2,5 +2,14 @@
 
 echo "---> Starting lighttpd..."
 
+# For POST/upload support
+
+mkdir /run/lighttpd/
+mkdir /run/lighttpd/upload
+
+chmod go+rw /run/lighttpd/
+chmod go+rw /run/lighttpd/upload
+chmod go+rw /var/tmp
+
 #httpd -h /home/www
 lighttpd -f /etc/lighttpd/lighttpd.conf
