b5f247bc6842dc9c5c32f9cfd17b8ae80cd5f93b
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-12-30 21:00:34 +0100

    LibGD

--- scripts/bs_graphic_stack.sh
+++ scripts/bs_graphic_stack.sh
@@ -855,3 +855,37 @@ then
 ) || exit 1
 fi
 
+####################################################################
+# LibGD
+####################################################################
+
+CUR_PACKAGE=${SRC_PACKAGE_LIBGD:-"UNDEF"}
+CUR_PACKAGE="${CUR_PACKAGE##*/}"
+if [ "$CUR_PACKAGE" != "UNDEF" ]
+then
+(
+	if [ ! -f ${TARGET_BUILD}/${CUR_PACKAGE}_DONE ]
+	then
+	(
+		unpack ${CUR_PACKAGE} ""
+
+		cd ${TARGET_BUILD} || exit 1
+		mkdir libgd
+		cd libgd || exit 1
+
+		${TARGET_SOURCES}/${TMP_ARCHIVE_FOLDER}/configure \
+				--prefix="${TARGET_ROOTFS}" \
+				--build=$MACHTYPE \
+				--host=$TGT_MACH \
+				--target=$TGT_MACH \
+				|| exit 1
+
+		make ${NBCORE} || exit 1
+		make ${NBCORE} install || exit 1
+
+		echo "" > ${TARGET_BUILD}/${CUR_PACKAGE}_DONE
+
+	) || exit 1
+	fi
+) || exit 1
+fi
