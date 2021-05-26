49c43096453fbb86f4962d151246518035456374
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2021-05-27 00:40:09 +0200

    rpi3 : kernel and configuration updated.

--- targets/rpi3/config/config.sh
+++ targets/rpi3/config/config.sh
@@ -16,8 +16,8 @@ export KERNEL_IMAGE_TYPE="Image"
 export DEBUG_SUPPORT="1"
 export NETWORK_SUPPORT="1"
 export WIRELESS_SUPPORT="1"
-export GFX_SUPPORT="1"
-export WAYLAND_SUPPORT="1"
+#export GFX_SUPPORT="1"
+#export WAYLAND_SUPPORT="1"
 export AUDIO_SUPPORT="1"
 export TARGET_BUILD_SUPPORT="1"
 #export GAMES_SUPPORT="1"
@@ -27,30 +27,31 @@ export NETWORK_STATION_MODE="1"
 
 source ${BASE_DIR}/targets/common/config/config.sh || exit 1
 
-export MESA_DRI_DRV="swrast"
-export MESA_GALLIUM_DRV="vc4,swrast"
-export DRM_SUPPORT="--enable-vc4"
+#export MESA_DRI_DRV="swrast"
+#export MESA_GALLIUM_DRV="vc4,swrast"
+#export DRM_SUPPORT="--enable-vc4"
 
 ###################################################
 # Cross compiler for the cross compiled machine
 
-export CROSS_TGT_MACH=arm-none-eabi
-export CROSS_TGT_CPU=cortex-m3
-export CROSS_TGT_CPUMODE=thumb
+#export CROSS_TGT_MACH=arm-none-eabi
+#export CROSS_TGT_CPU=cortex-m3
+#export CROSS_TGT_CPUMODE=thumb
 
-SRC_PACKAGE_TARGET_CROSS_BINUTILS="http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
+#SRC_PACKAGE_TARGET_CROSS_BINUTILS="http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
 
-SRC_PACKAGE_TARGET_CROSS_GCC="http://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.gz"
-SRC_PACKAGE_TARGET_CROSS_GCC_MPFR="http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.xz"
-SRC_PACKAGE_TARGET_CROSS_GCC_GMP="http://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.xz"
-SRC_PACKAGE_TARGET_CROSS_GCC_MPC="http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
-SRC_PACKAGE_TARGET_CROSS_GCC_ISL="ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2"
-SRC_PACKAGE_TARGET_CROSS_GCC_CLOOG="ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz"
+#SRC_PACKAGE_TARGET_CROSS_GCC="http://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.gz"
+#SRC_PACKAGE_TARGET_CROSS_GCC_MPFR="http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.xz"
+#SRC_PACKAGE_TARGET_CROSS_GCC_GMP="http://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.xz"
+#SRC_PACKAGE_TARGET_CROSS_GCC_MPC="http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
+#SRC_PACKAGE_TARGET_CROSS_GCC_ISL="ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2"
+#SRC_PACKAGE_TARGET_CROSS_GCC_CLOOG="ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz"
 
 ###################################################
 # Kernel
 
-SRC_PACKAGE_KERNEL="https://github.com/raspberrypi/linux/archive/raspberrypi-kernel_1.20180417-1.tar.gz"
+SRC_PACKAGE_KERNEL="https://github.com/raspberrypi/linux/archive/refs/tags/raspberrypi-kernel_1.20210430-1.tar.gz"
 
 SRC_PACKAGE_DIRECTFB=
 
+SRC_PACKAGE_STRACE=
