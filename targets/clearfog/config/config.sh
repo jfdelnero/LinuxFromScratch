c13bd7d2dbe627de90dae4ee7686d43fdb090de5
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2022-10-31 18:39:36 +0100

    target cross compiler updated.

--- targets/clearfog/config/config.sh
+++ targets/clearfog/config/config.sh
@@ -34,11 +34,11 @@ export CROSS_TGT_CPUMODE=thumb
 
 SRC_PACKAGE_TARGET_CROSS_BINUTILS="http://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
 
-SRC_PACKAGE_TARGET_CROSS_GCC="http://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.gz"
-SRC_PACKAGE_TARGET_CROSS_GCC_MPFR="http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.xz"
-SRC_PACKAGE_TARGET_CROSS_GCC_GMP="http://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.xz"
-SRC_PACKAGE_TARGET_CROSS_GCC_MPC="http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz"
-SRC_PACKAGE_TARGET_CROSS_GCC_ISL="ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.15.tar.bz2"
+SRC_PACKAGE_TARGET_CROSS_GCC="https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+SRC_PACKAGE_TARGET_CROSS_GCC_MPFR="https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz"
+SRC_PACKAGE_TARGET_CROSS_GCC_GMP="https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz"
+SRC_PACKAGE_TARGET_CROSS_GCC_MPC="https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz"
+SRC_PACKAGE_TARGET_CROSS_GCC_ISL="ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2"
 SRC_PACKAGE_TARGET_CROSS_GCC_CLOOG="ftp://gcc.gnu.org/pub/gcc/infrastructure/cloog-0.18.1.tar.gz"
 
 SRC_PACKAGE_CVS="https://ftp.gnu.org/non-gnu/cvs/source/stable/1.11.23/cvs-1.11.23.tar.gz"
@@ -48,3 +48,4 @@ source ${BASE_DIR}/targets/common/config/config.sh || exit 1
 # Kernel
 
 SRC_PACKAGE_KERNEL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.17.tar.xz"
+
