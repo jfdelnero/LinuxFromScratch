6933034b898e8f4929fd7d463b1a9d91cd2ccdf1
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2022-11-01 10:20:41 +0100

    private root fs.

--- targets/clearfog/config/install_post_process.sh
+++ targets/clearfog/config/install_post_process.sh
@@ -86,6 +86,11 @@ sudo cp -av ${TARGET_ROOTFS_MIRROR}/* ${TARGET_HOME}/output_objects/tmp_mount_po
 #sudo mkimage -A arm -T script -O linux -d ${TARGET_CONFIG}/boot_part/boot.script ${TARGET_HOME}/output_objects/tmp_mount_point/boot.scr
 sudo cp -av ${TARGET_CONFIG}/boot_part/boot.scr  ${TARGET_HOME}/output_objects/tmp_mount_point/  || exit 1
 
+if [[ -f ${TARGET_CONFIG}/private_rootfs/ ]]
+then
+	sudo cp -av ${TARGET_CONFIG}/private_rootfs_cfg/       ${TARGET_HOME}/output_objects/tmp_mount_point/  || exit 1
+fi
+
 #sudo cp -av ${TARGET_HOME}/output_objects/a_new_program  ${TARGET_HOME}/output_objects/tmp_mount_point/usr/sbin || exit 1
 
 ###############################################################################
