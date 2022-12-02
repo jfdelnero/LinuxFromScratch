5e1e6f15a9222ea987735af2d63e9aace71105ea
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2022-12-02 23:13:53 +0100

    2GB file system + various fixes.

--- targets/rpi3/config/rootfs_cfg/etc/rcS.d/S10_filesystems.sh
+++ targets/rpi3/config/rootfs_cfg/etc/rcS.d/S10_filesystems.sh
@@ -13,10 +13,12 @@ mount -t sysfs none /sys
 echo mmc0 | tee /sys/class/leds/led0/trigger
 
 mount -t tmpfs -o size=256m tmpfs /root
-chmod go-r /root
+chmod go-rwx /root
 
 mount -t tmpfs -o size=64m  tmpfs /tmp
+
 mount -t tmpfs -o size=64m  tmpfs /var
+chmod og-w /var
 
 # Populate /var
 mkdir /var/log
@@ -31,12 +33,29 @@ mkdir /var/log/lastlog
 mkdir /var/tmp
 mkdir /var/local
 mkdir /var/lib
+mkdir /var/cache
 mkdir /var/lock
 mkdir /var/spool
 mkdir /var/spool/cron
 mkdir /var/spool/cron/crontabs
 mkdir /var/state
 mkdir /var/mail
+mkdir /var/cache/samba/
+mkdir /var/lock/samba
+mkdir /var/log/samba
+mkdir /var/lib/samba/
+mkdir /var/lib/samba/private/
+
+chgrp smbuser /var/log/samba
+chown smbuser /var/log/samba
+chgrp smbuser /var/cache/samba
+chown smbuser /var/cache/samba
+chgrp smbuser /var/lock/samba
+chown smbuser /var/lock/samba
+chgrp smbuser /var/lib/samba
+chown smbuser /var/lib/samba
+chgrp smbuser /var/lib/samba/private
+chown smbuser /var/lib/samba/private
 
 #######################################
 # /dev
@@ -46,6 +65,12 @@ mkdir /var/mail
 #mount -t tmpfs -o size=64m  tmpfs /mnt
 #cp -aR /dev/* /mnt
 #mount --move /mnt /dev
+
+echo /sbin/mdev > /proc/sys/kernel/hotplug
+/sbin/mdev -s
+
+# trigger the watchdog
+#echo w > /dev/watchdog
 
 # video / drm device
 if [ ! -f "/dev/dri/card0" ]
@@ -76,19 +101,26 @@ chmod ugo+rw /dev/tty
 mount -t tmpfs -o size=64m  tmpfs /mnt
 cp -aR /home/* /mnt
 mount --move /mnt /home
-mkdir /home/data
+chmod og-w /home
 
 #######
 # /run
 mount -t tmpfs -o size=64m  tmpfs /mnt
 cp -aR /run/* /mnt 2> /dev/null
 mount --move /mnt /run
+chmod og-w /run
 
 #######
 # /etc
 mount -t tmpfs -o size=64m  tmpfs /mnt
 cp -aR /etc/* /mnt
 mount --move /mnt /etc
+chmod og-w /etc
+
+mount -t tmpfs -o size=500m none /ramdisk
+chown ramdisk /ramdisk
+chgrp ramdisk /ramdisk
+chmod og-rwx /ramdisk
 
 #######################################
 # Test the data partition presence
@@ -99,8 +131,8 @@ if [ $SIZELEN == 0 ]; then
    #/usr/sbin/splash_screen /data/pauline_splash_bitmaps/prep_data_disk.bmp
    echo "Partition 3 not defined... Add it"
 
-   # Add a partition (exfat) - (2097152 sectors -> keep/reserved the first 1GB for the system partitions)
-   echo 'start=2097152,type=7' | sfdisk /dev/mmcblk0 -f -N 3
+   # Add a partition (exfat) - (4194304 sectors -> keep/reserved the first 2GB for the system partitions)
+   echo 'start=4194304,type=7' | sfdisk /dev/mmcblk0 -f -N 3
 
    # And restart the system...
    #/usr/sbin/splash_screen /data/pauline_splash_bitmaps/rebooting.bmp
@@ -159,13 +191,5 @@ mount -o fmask=0000,dmask=0000 /dev/mmcblk0p3 /home/data
 
 echo 100 > /proc/sys/vm/dirty_expire_centisecs
 echo 100 > /proc/sys/vm/dirty_writeback_centisecs
-
-#######################################
-# ramdisk
-#######################################
-
-mount -t tmpfs -o size=64m  tmpfs /ramdisk
-cp -aR /ramdisk/* /mnt
-mount --move /mnt /ramdisk
 
 mount -a
