99f684e14033066d54e7bd3962735d6e8f80b297
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2025-01-26 21:05:45 +0100

    ia32 efi + x86_64 efi + bios boot support.

--- targets/pc-686/config/install_post_process.sh
+++ targets/pc-686/config/install_post_process.sh
@@ -28,7 +28,7 @@ fdisk -l ${TARGET_HOME}/output_objects/hdd.img
 sudo losetup --show --sector-size 512 -f -P ${TARGET_HOME}/output_objects/hdd.img
 
 # efi partion -> FAT
-sudo mkfs.vfat /dev/loop0p2
+sudo mkfs.vfat -F32 /dev/loop0p2
 
 # System partition
 sudo mkfs.ext2 /dev/loop0p3
@@ -145,7 +145,62 @@ then
 
 	make ${MAKE_FLAGS} ${NBCORE}         || exit 1
 
+	sudo ./grub-install --target=x86_64-efi --directory=grub-core --efi-directory=${TARGET_HOME}/output_objects/tmp_mount_point2 --bootloader-id=GRUB --modules="normal part_msdos part_gpt multiboot" --root-directory=${TARGET_HOME}/output_objects/tmp_mount_point --no-floppy /dev/loop0 || exit 1
+
+	sudo mkdir  ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/BOOT || exit 1
+	sudo mv     ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/GRUB/grubx64.efi ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/BOOT/BOOTX64.EFI || exit 1
+	sudo rm -rf ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/GRUB || exit 1
+	#make ${MAKE_FLAGS} ${NBCORE} install || exit 1
+
+	delete_src_dir
+
+	echo "" > ${TARGET_BUILD}/${CUR_PACKAGE}_DONE
+) || exit 1
+fi
+
+CUR_PACKAGE=${SRC_PACKAGE_GRUB_EFI:-"UNDEF"}
+CUR_PACKAGE="${CUR_PACKAGE##*/}"
+if [ "$CUR_PACKAGE" != "UNDEF" ]
+then
+(
+	create_src_dir
+
+	unpack ${CUR_PACKAGE} ""
+
+	cd ${TMP_SRC_FOLDER}/${TMP_ARCHIVE_FOLDER} || exit 1
+
+	if [ -f ${TARGET_CONFIG}/patches/grub.patch ]
+	then
+	(
+		cd conf || exit 1
+		patch -Zf < ${TARGET_CONFIG}/patches/grub.patch  || exit 1
+	) || exit 1
+	fi
+
+	cd ${TMP_SRC_FOLDER}/${TMP_ARCHIVE_FOLDER} || exit 1
+
+	unset PKG_CONFIG_LIBDIR
+
+	export GRUB_AUTORECONF=1
+	export GRUB_AUTOGEN=1
+
+	echo depends bli part_gpt > ./grub-core/extra_deps.lst || exit 1
+
+	${TMP_SRC_FOLDER}/${TMP_ARCHIVE_FOLDER}/configure \
+			--prefix="${BUILDTOOLS_HOME}" \
+			--disable-werror \
+			--target=${TGT_MACH} \
+			--with-platform=efi \
+			|| exit 1
+
+	./autogen.sh ||  exit 1
+
+	make ${MAKE_FLAGS} ${NBCORE}         || exit 1
+
 	sudo ./grub-install --target=i386-efi --directory=grub-core --efi-directory=${TARGET_HOME}/output_objects/tmp_mount_point2 --bootloader-id=GRUB --modules="normal part_msdos part_gpt multiboot" --root-directory=${TARGET_HOME}/output_objects/tmp_mount_point --no-floppy /dev/loop0 || exit 1
+
+	sudo mv     ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/GRUB/grubia32.efi ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/BOOT/BOOTIA32.EFI || exit 1
+	sudo rm -rf ${TARGET_HOME}/output_objects/tmp_mount_point2/EFI/GRUB || exit 1
 
 	#make ${MAKE_FLAGS} ${NBCORE} install || exit 1
 
@@ -205,6 +260,17 @@ then
 ) || exit 1
 fi
 
+
+###############################################################################
+# owner / group / right accesses fix
+
+cd ${TARGET_HOME}/output_objects/tmp_mount_point/ || exit 1
+
+${SCRIPTS_HOME}/fix_fs_perm.sh
+
+sudo chown 1001 ${TARGET_HOME}/output_objects/tmp_mount_point/ramdisk
+sudo chgrp 1001 ${TARGET_HOME}/output_objects/tmp_mount_point/ramdisk
+sudo chmod o+wr ${TARGET_HOME}/output_objects/tmp_mount_point/ramdisk
 
 sudo umount ${TARGET_HOME}/output_objects/tmp_mount_point
 sudo umount ${TARGET_HOME}/output_objects/tmp_mount_point2
